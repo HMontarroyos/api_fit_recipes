c7e4a98068988913039a6f265802cfb5e96aa5ca
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-17 13:04:04 -0300

    feat: Add MongoDB Atlas integration for database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,21 @@
 import mongoose, { ConnectOptions } from "mongoose";
+import { config as dotenvConfig } from 'dotenv';
+dotenvConfig();
 
 export async function connect() {
+  const baseUrlHML = "mongodb://127.0.0.1:27017/recipes"
+  const baseUrlPROD = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_URL}/${process.env.MONGODB_DATABASE}`
   try {
-    const options : any = {};
+    const options: any = {};
     options.useNewUrlParser = true;
     options.useUnifiedTopology = true;
-    await mongoose.connect("mongodb://127.0.0.1:27017/recipes", options);
+    await mongoose.connect(baseUrlPROD, options);
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error);
   }
+}
+
+export async function closeDatabase() {
+  await mongoose.connection.close();
 }
