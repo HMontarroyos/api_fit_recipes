bdf62e2a2205435eb9e4aa17a77dc8c7429359cd
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-18 22:11:55 -0300

    feat: Create new recipe model

--- src/modules/Recipes/models/recipe.ts
+++ src/modules/Recipes/models/recipe.ts
@@ -2,22 +2,58 @@ import { Document, Schema, model } from "mongoose";
 
 export interface Recipe extends Document {
   name: string;
-  description: string;
-  ingredients: string[];
-  instructions: string;
+  description?: string;
+  ingredients: {
+    amount: string;
+    name: string;
+    measurement: string;
+  }[];
+  instructions: {
+    step: string;
+    description: string;
+  }[];
   image: string;
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
+  rating?: number;
+  timer: string;
+  portion: string;
+  difficulty: string;
+  type: string;
+  occasions: string;
+  specialOccasions?: string;
+  utensils?: string;
+  foodRestriction?: string;
 }
 
 const RecipeSchema: Schema = new Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
-  ingredients: [{ type: String, required: true }],
-  instructions: { type: String, required: true },
+  description: { type: String, required: false },
+  ingredients: [
+    {
+      amount: { type: String, required: true },
+      name: { type: String, required: true },
+      measurement: { type: String, required: true },
+    },
+  ],
+  instructions: [
+    {
+      step: { type: String, required: true },
+      description: { type: String, required: true },
+    },
+  ],
   image: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  rating: { type: Number, required: false },
+  timer: { type: String,  required: true },
+  portion: { type: String, required: true },
+  difficulty: { type: String, required: true },
+  type: { type: String, required: true },
+  occasions: { type: String, required: true },
+  specialOccasions: { type: String, required: false },
+  utensils: { type: String, required: false },
+  foodRestriction: { type: String, required: false },
 });
 
 export default model<Recipe>("Recipe", RecipeSchema);
